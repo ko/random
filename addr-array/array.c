cb3911e956b2ea31663c7b87a310f1eb07d103a4
ko <k1ko@scu.edu>
2011-09-22 00:18:50 -0700

    more intruded.net material

---
+++ addr-array/array.c
@@ -0,0 +1,11 @@
+/*
+ *	address of a particular string / buffer in memory
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+        printf("%x\n", argv[1]);
+}	
