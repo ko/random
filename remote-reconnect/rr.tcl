493f2578e98504e9b966e0df24eb8629dc29b196
Ken Ko <ko.harmony@gmail.com>
2011-11-23 20:11:47 -0600

    Cleanup of useless files.

--- remote-reconnect/rr.tcl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect -f
-
-set ip [lindex $argv 0]
-set username [lindex $argv 1]
-set password [lindex $argv 2]
-spawn telnet -l $username $ip
-expect -re "Password:"
-send "sunStone\n"
-interact
