9302f2992e18f3c2493a69dfea2278036b69888b
ko <k1ko@scu.edu>
2011-12-17 15:49:32 -0800

    page 37/49

--- book-examples/linux-device-drivers-3/ch17/snull.c
+++ book-examples/linux-device-drivers-3/ch17/snull.c
@@ -136,3 +136,183 @@ int snull_tx(struct sk_buff *skb, struct net_device *dev)
     return 0;
 }
 
+/* simulating transmitter lockups */
+static int lockup = 0;
+module_param(lockup, int, 0);   /* simul. every n packets tx'd */     
+
+static int timeout = SNULL_TIMEOUT;
+module_param(timeout, int, 0);  
+
+/* snull transmission timeout handler 
+ *
+ * when transmission timeout happens, driver must mark the
+ * error in the interface stats and arrange for device to be 
+ * reset to a sane state so new pkts can be tx'd. 
+ */
+void snull_tx_timeout(struct net_device *dev)
+{
+    struct snull_priv *priv = netdev_priv(dev);
+    PDEBUG("Transmit timeout at %ld, latency %ld\n", jiffies,
+           jiffies - dev->trans_start);
+    /* simulate a transmission interrupt to get things moving */
+    priv->status = SNULL_TX_INTR;
+    /* fills in missing interrupt */ 
+    snull_interrupt(0, dev, NULL);
+    priv->stats.tx_errors++;
+    /* restart the tx queue */
+    netif_wake_queue(dev);
+    return;
+}
+
+void snull_rx(struct net_device *dev, struct snull_packet *pkt)
+{
+    struct sk_buff *skb;
+    struct snull_priv *priv = netdev_priv(dev);
+
+    /* the packet has been retrieved from the tx
+     * medium. build an skb around it so upper layers
+     * may have at it
+     */
+    skb = dev_alloc_skb(pkt->datalen + 2); /* kmalloc with atomicity */
+    if (!skb) {
+        if (printk_ratelimit())
+            printk(KERN_NOTICE "snull rx: low on mem - packet dropped\n");
+        priv->stats.rx_dropped++;
+        goto out;
+    }
+    memcpy(skb_put(skb, pkt->datalen), pkt->data, pkt->datalen);
+
+    /* write metadata, pass to receive level */
+    skb->dev = dev;
+    skb->protocol = eth_type_trans(skb, dev);
+    skb->ip_summed = CHECKSUM_UNNECESSARY; /* no need to check it */
+    priv->stats.rx_packets++;
+    priv->stats.rx_bytes += pkt->datalen;
+    netif_rx(skb); 
+out:
+    return;
+}
+
+
+static void snull_regular_interrupt(int irq, void *dev_id, struct pt_regs *regs)
+{
+    int statusword;
+    struct snull_priv *priv;
+    struct snull_packet *pkt = NULL;
+
+    /* first task is to retrieve a ptr to the correct struct net_device
+     *
+     * as usual, check the 'device' ptr to be sure
+     * it is really interrupting. 
+     *
+     * then assign "struct device *dev"
+     */
+    struct net_device *dev = (struct net_device *)dev_id;
+    /* ... and check with hw if it's really ours */
+
+    /* paranoid */
+    if (!dev)
+        return;
+
+    /* lock the dev */
+    priv = netdev_priv(dev);
+    spin_lock(&priv->lock);
+
+    /* retrieve statusword: real netdevices use IO instructions */
+    statusword = priv->status;
+    priv->status = 0;
+    if (statusword & SNULL_RX_INTR) {
+        /* send it to snull_rx for handling */
+        pkt = priv->rx_queue;
+        if (pkt) {
+            priv->rx_queue = pkt->next;
+            snull_rx(dev, pkt);
+        }
+    }
+    if (statusword & SNULL_TX_INTR) {
+        /* transmission is over: 
+         *  update stats
+         *  free the skb */
+        priv->stats.tx_packets++;
+        priv->stats.tx_bytes += priv->tx_packetlen;
+        /* dev_kfree_skb
+         *  - called only when you know code will not run in interrupt context
+         *  (snull has no h/w interrupts)
+         *
+         * dev_kfree_skb_irq
+         *  - optimized for freeing the buffer in an interrupt handler 
+         *
+         * dev_kfree_skb_any 
+         *  - used for both in-and-out of int/nonint contexts
+         */
+        dev_kfree_skb(priv->skb);
+    }
+    /* if tx queue paused, restart with netif_wake_queue here */
+
+    /* unlock the device and we are done */
+    spin_unlock(&priv->lock);
+    if (pkt) snull_release_buffer(pkt); /* do this outside the lock */
+    return;
+}
+
+/*
+ * budget:  max no. of pkts allowed to pass into the kernel
+ *          max no. of pkts that the current CPU can rx from all interfaces
+ */
+static int snull_poll(struct net_device *dev, int *budget)
+{
+    /* dev->quota: per-interface value */ 
+    int npackets = 0, quota = min(dev->quota, *budget);
+    struct sk_buff *skb;
+    struct snull_priv *priv = netdev_priv(dev);
+    struct snull_packet *pkt;
+
+    while (npackets < quota && priv->rx_queue) {
+        pkt = snull_dequeue_buf(dev);
+        skb = dev_alloc_skb(pkt->datalen + 2);
+        if (!skb) {
+            if (printk_ratelimit())
+                printk(KERN_NOTICE "snull: packet dropped\n");
+            priv->stats.rx_dropped++;
+            snull_release_buffer(pkt);
+            continue;
+        }
+        memcpy(skb_put(skb, pkt->datalen), pkt->data, pkt->datalen);
+        skb->dev = dev;
+        skb->protocol = eth_type_trans(skb, dev);
+        skb->ip_summed = CHECKSUM_UNNECESSRY; /* don't check */
+        /* use instead of netif_rx */
+        netif_receive_skb(skb);
+
+        /* stat maintenance */
+        npackets++;
+        priv->stats.rx_packets++;
+        priv->stats.rx_bytes += pkt->datalen;
+        snull_release_buffer(skb);
+    }
+    /* if all pkts processed, done; 
+     * tell kernel + reenable ints */
+    *budget -= npackets;
+    dev->quota -= npackets; /* consistency */
+    if (!priv->rx_queue) {
+        netif_rx_complete(dev);
+        snull_rx_ints(dev, 1);
+        return 0;
+    }
+    /* couldn't process everything :( */
+    return 1;
+}
+
+int snull_header(struct sk_buff *skb, strut net_device *dev,
+                unsigned short type, void *daddr, void *saddr,
+                unsigned int len)
+{
+    struct ethhdr *eth = (struct ethhdr *)skb_push(skb,ETH_HLEN);
+
+    eth->h_proto = htons(type);
+    memcpy(eth->h_source, saddr ? saddr : dev->dev_addr, dev->addr_len);
+    memcpy(eth->h_dest, daddr ? daddr : dev->dev_addr, dev->addr_len);
+    eth->h_dest[ETH_ALEN-1] ^= 0x01;    /* dest is us xor 1 */ 
+    return  (dev->hard_header_len);
+}
+
